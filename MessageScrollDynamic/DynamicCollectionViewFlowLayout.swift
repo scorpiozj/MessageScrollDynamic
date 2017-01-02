f02aeb5421c7ba248aa46328972e3b56db1eca41
Zhu Jiang <scorpiozj@gmail.com>
2017-01-02 14:18:17 +0800

    Add init add

---
+++ MessageScrollDynamic/DynamicCollectionViewFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  DynamicCollectionViewFlowLayout.swift
+//  MessageScrollDynamic
+//
+//  Created by ZhuJiang on 17/1/2.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+import UIKit
+
+class DynamicCollectionViewFlowLayout: UICollectionViewFlowLayout {
+
+}
