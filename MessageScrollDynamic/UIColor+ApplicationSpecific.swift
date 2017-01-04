6ae9ee1e3a2b5fde9d3467749b127b4405f82782
Zhu Jiang <scorpiozj@gmail.com>
2017-01-04 13:14:43 +0800

    添加UIColor的extension

---
+++ MessageScrollDynamic/UIColor+ApplicationSpecific.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+ApplicationSpecific.swift
+//  MessageScrollDynamic
+//
+//  Created by ZhuJiang on 17/1/4.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var applicationBlue: UIColor {
+        return UIColor(red: 0.659, green: 0.271, blue: 0.988, alpha: 1.0)
+    }
+    
+}
